42c887a0ab702b64ba3a9119116a66269d8bc391
michaelagbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2022-10-02 20:36:27 +0100

    Update main.py
    Signed-off-by: michaelagbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>

--- day-4/project/main.py
+++ day-4/project/main.py
@@ -30,7 +30,7 @@ scissors = '''
 all = [rock, paper, scissors]
 
 ram = random.choice(all)
-print(ram)
+# print(ram)
 
 inPut = input("What do you choose::: 0 for rock, 1 for paper or 2 for scissors\n")
 
