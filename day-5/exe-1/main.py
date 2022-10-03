f1e6decae7cb2d6877052b11adab949f0fab71b0
michaelagbiaowei <bryanwalton1997@gmail.com>
2022-10-03 16:22:26 +0100

    updated

---
+++ day-5/exe-1/main.py
@@ -0,0 +1,14 @@
+student_height = input("Input a list of student heights\n").split()
+
+for n in range(0, len(student_height)):
+    student_height[n] = int(student_height[n])
+
+# print(student_height)
+count = 0
+total = 0
+
+for num in student_height:
+    total += num
+    count += 1
+average = total / count 
+print(round(average))
