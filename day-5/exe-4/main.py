6a7593b1875c8798d33dd62fb5eaf393c11c21fe
michaelagbiaowei <bryanwalton1997@gmail.com>
2022-10-04 13:23:34 +0100

    updated

--- day-5/exe-4/main.py
+++ day-5/exe-4/main.py
@@ -2,10 +2,10 @@
 
 for n in range(1, 101):
     if n % 3 == 0 and n % 5 == 0:
-        print("Buzz")
+        print("FizzBuzz")
     elif n % 3 == 0:
         print("Fizz")
     elif n % 5 == 0:        
-        print("FizzBuzz")
+        print("Buzz")
     else:
         print(n)
