0f1c735970d8009838798e32fa16d4deaa94bb04
michaelagbiaowei <bryanwalton1997@gmail.com>
2022-10-05 16:22:16 +0100

    updated

---
+++ day-6/exe-2/main.py
@@ -0,0 +1,16 @@
+def stage():
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+ 
+while at_goal() != True:
+    stage()
