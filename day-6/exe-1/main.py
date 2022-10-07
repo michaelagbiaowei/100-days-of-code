64983638ed98ffb12a9ea36c3356623b9414aa4d
michaelagbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2022-10-07 15:36:15 +0100

    Update main.py
    Signed-off-by: michaelagbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>

--- day-6/exe-1/main.py
+++ day-6/exe-1/main.py
@@ -13,3 +13,21 @@ def stage():
     turn_left()   
 for x in range(0, 6):
     stage()
+
+#     def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# for n in range(0, 6):
+#     jump()
