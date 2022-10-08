3d3eb060519e21d67269fcbe5751d5ffe8f47e8c
michaelagbiaowei <bryanwalton1997@gmail.com>
2022-10-08 12:31:14 +0100

    updated

--- day-7/exe-1/main.py
+++ day-7/exe-1/main.py
@@ -3,7 +3,7 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
-print(chosen_word)
+# print(chosen_word)
 
 guess = input("Guess a Letter\n").lower()
 
